11162f33ecc895ce8b805fca6d2b55df5f893aab
BADLATTE <badlatte@BADLATTEs-MacBook-Air.local>
2018-05-05 17:21:20 -0700

    first commit

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var usermodel = require('./user.js').getModels();
 var homeworkmodel = require('./hmwkuser1.js').getModels();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var crypto = require('crypto');
 var Io = require('socket.io');
+var session = require('express-session');
+
 
 
 /* The http module is used to listen for requests from a web browser */
@@ -41,20 +45,36 @@ function startServer() {
 		function authenticateUser(userName, password, callback) {
 			if(!userName) return callback('Please enter a username!');
 			if(!password) return callback('Please enter a password!');
-			homeworkmodel.findOne({userName: userName}, function(err, user){
+			homeworkmodel.findOne({userName: userName}, (err, user) => {
 				if(err) return callback('Make sure you entered the user correctly');
 				if(!user) return callback('Username not found');
-				crypto.pbkdf2(password, user.salt, 10000, 256, 'sha256', function(err, hash) {
-					if(err) return callback('Error hashing password')
-					if(password === hash.toString('base64')) return callback('Wrong password');
-					callback(null);
+				crypto.pbkdf2(password, user.salt, 10000, 256, 'sha256', function(err, resp) {
+					if(err) return callback('Error hashing password');
+					if(resp.toString('base64') === user.password) return callback(null, user);
+					callback('Incorrect password');
 				});
 			});
 		}
 		addSockets();
 		app.use(bodyParser.json({ limit: '16mb' }));
 		app.use(express.static(path.join(__dirname, 'public')));
+		app.use(session({ secret: 'perishfool'}));
+		app.use(passport.initialize());
+		app.use(passport.session());
 
+
+		passport.use(new LocalStrategy({usernameField: 'userName', passwordField: 'password'}, authenticateUser));
+
+		passport.serializeUser(function(user, done){
+			console.log('serialize user')
+			done(null, user.id);
+		});
+		passport.deserializeUser(function(id, done){
+			homeworkmodel.findById(id, function(err, user){
+				console.log('deserialize user', id, user);
+				done(err, user);
+			});
+		});
 		/* Defines what function to call when a request comes from the path '/' in http://localhost:8080 */
 		app.get('/form', (req, res, next) => {
 
@@ -106,15 +126,25 @@ function startServer() {
 		});
 
 		app.post('/login', (req, res, next) => {
-			var userName = req.body.userName;
-			var password = req.body.password;
-
-			authenticateUser(userName, password, function(err){
-				res.send({error: err});
-			});
+			console.log('HERE')
+			passport.authenticate('local', function(err, user){
+				if(err) return res.send({error: err});
+				req.logIn(user, (err) => {
+					if(err) return res.send({error:err});
+					return res.send({error: null})
+				});
+			})(req, res, next);
 		});
-		app.get('/game', (req, res, next) => {
 
+		app.get('/logout', (req, res, next) => {
+			req.logOut();
+			res.redirect('/login?error=S%20T%20R%20I%20P%20E%20S');
+		});
+
+		app.get('/game', (req, res, next) => {
+			if(!req.user) {
+				return res.redirect('/login?error=PERISH%20FOOL');
+			}
 			/* Get the absolute path of the html file */
 			var filePath = path.join(__dirname, './game.html')
 
